a63027e6835beb46fd261520b2836bf6291377a8
Ian Kenney <ikenney@asu.edu>
2023-01-18 13:57:44 -0700

    Removed import of canvas funcation from __init__.py

--- mdakithole2/__init__.py
+++ mdakithole2/__init__.py
@@ -3,9 +3,6 @@ mdakithole2
 A Python interface for the HOLE suite tools to analyze an ion channel pore or transporter pathway as a function of time or arbitrary order parameters.
 """
 
-# Add imports here
-from .mdakithole2 import canvas
-
 # Handle versioneer
 from ._version import get_versions
 versions = get_versions()
