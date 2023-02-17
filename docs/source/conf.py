f109d0aa1338d85a456e20a5d2ebeb9398407e08
Ian Kenney <ianmkenney@users.noreply.github.com>
2023-02-17 14:48:49 -0700

    Deploy documenation on github pages (#11)
    * Pushes to main branch deploy docs to the gh-pages branch

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,12 +13,12 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 
 # Incase the project was not installed
-import mdakithole2
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
 
+import mdakithole2
 # -- Project information -----------------------------------------------------
 
 project = 'mdakithole2'
@@ -79,7 +79,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = "en"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
